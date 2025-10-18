d2a279a1591d9fd1b9a7f6abb7bade0574d838a1
syspenc199 <sy.spenc199@gmail.com>
2025-10-18 11:09:56 -0700

    Implement DrawMesh method in MapDisplay class
    Added DrawMesh method to handle mesh rendering with texture.

--- MapDisplay.cs
+++ MapDisplay.cs
@@ -3,9 +3,17 @@ using UnityEngine;
 public class MapDisplay : MonoBehaviour
 {
     public Renderer renderer;
+    public MeshFilter meshFilter;
+    public MeshRenderer meshRender;
 
     public void DrawTexture(Texture2D texture)
     {
         renderer.sharedMaterial.mainTexture = texture;
+    }
+
+    public void DrawMesh(MeshData meshData, Texture2D texture)
+    {
+        meshFilter.sharedMesh = meshData.CreateMesh();
+        meshRender.sharedMaterial.mainTexture = texture;
     }
 }
